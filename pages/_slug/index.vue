d7a4e728f15d47562683f1fa67e56543b9c01e91
kkoouuya <kouya.0730@icloud.com>
2022-10-16 19:22:10 +0900

    first commit

---
+++ pages/_slug/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://roasting.microcms.io/api/v1/blogs/${params.slug}`,
+      {
+        headers: {
+          "X-MICROCMS-API-KEY": "c43a04daca4f4e96b9397b0add526afead37",
+        },
+      }
+    );
+    return data;
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "IndexPage",
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      "https://roasting.microcms.io/api/v1/blogs",
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: {
+          "X-MICROCMS-API-KEY": "c43a04daca4f4e96b9397b0add526afead37",
+        },
+      }
+    );
+    return data;
+  },
+};
+</script>
